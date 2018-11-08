0b3e65fb15375ce54d8ceb79bdc902d3c42780b8
Kanarpp <lecarnetduvagabond@gmail.com>
2018-11-08 19:19:34 +0100

    Ajout css

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+include('inc/pdo.php');
+include('inc/fonctions.php');
+include('inc/header.php') ?>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h3>Oops! Page non trouvée</h3>
+				<h2><span>4</span><span>0</span><span>4</span></h2>
+			</div>
+			<p>Nous sommes désolé, nous ne trouvons pas la page</p>
+		</div>
+	</div>
+
+
+	<?php include('inc/footer.php') ?>
