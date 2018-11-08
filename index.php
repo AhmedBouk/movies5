78be21eef7823eeb3b73cf5d2ca5d794842fe1bb
camillelupo <camille.lupo@hotmail.fr>
2018-11-08 12:13:25 +0100

    acceuil 08112018 12h13

--- index.php
+++ index.php
@@ -3,15 +3,16 @@ include('inc/pdo.php');
 include('inc/fonctions.php');
 
 
-
-include('inc/header.php');
 $title = 'Home';
+include('inc/header.php');
 
 
 
-
-
-
+$sql = "SELECT * FROM movies_full ORDER BY rand() LIMIT 50 ";
+$query = $pdo -> prepare($sql);
+$query -> execute();
+$movies = $query -> fetchall();
+// debug($movies);
 
 
 
@@ -19,10 +20,23 @@ $title = 'Home';
 ?>
 <!-- Il y a une div id container autour du body  -->
 
+<!-- <a href="detail.php?id= -->
+
+<div class="films">
+  <?php
+    foreach ($movies as $movie) {
+      echo '<a href="detail.php?id="'.$movie['id'].'"><img src="posters/'.$movie['id'].'.jpg" alt="'.$movie['title'].'"></a>';
+    }
+
+   ?>
+</div>
+
+
+<?php
 
 
 
-
+  ?>
 
 
 
