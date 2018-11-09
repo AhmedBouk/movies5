07204b189421131da238fbf23cdc233d53910a7e
camillelupo <camille.lupo@hotmail.fr>
2018-11-09 09:41:16 +0100

    09/11 0941

--- index.php
+++ index.php
@@ -5,12 +5,20 @@ include('inc/fonctions.php');
 
 $title = 'Home';
 
+// Recuperation des donnes de la table movies_full
+$sql = "SELECT * FROM movies_full ORDER BY rand() LIMIT 50 ";
+$query = $pdo -> prepare($sql);
+$query -> execute();
+$movies = $query -> fetchall();
+
+
+//Recuperation  des catégories de la table movies_full
 $sql ="SELECT genres FROM movies_full";
 $query = $pdo -> prepare($sql);
 $query -> execute();
 $genres = $query -> fetchall();
 
-// debug($genres);
+// Tableau qui ne contient qu'une fois les differents genres
 $tableau = array();
 foreach ($genres as $genre) {
   $g = $genre['genres'];
@@ -24,36 +32,74 @@ foreach ($genres as $genre) {
     }
   }
 }
+//soumission du formulaire
+  if (!empty($_POST['submitted'])) {
+    $actif = TRUE;
 
-// debug($tableau);
-$sql = "SELECT * FROM movies_full ORDER BY rand() LIMIT 50 ";
-$query = $pdo -> prepare($sql);
-$query -> execute();
-$movies = $query -> fetchall();
-// debug($movies);
+    $choixDate = explode("-", $_POST['year']);
+    $sql = "SELECT * FROM movies_full WHERE year BETWEEN :annee1 AND :annee2";
+    $query = $pdo -> prepare($sql);
+    $query -> bindValue(':annee1', $choixDate[0], PDO::PARAM_STR);
+    $query -> bindValue(':annee2', $choixDate[1], PDO::PARAM_STR);
+    $query -> execute();
+    $filmSelection = $query -> fetchAll();
+
+debug($filmSelection);
+  }
+  else {
+    $actif = FALSE;
+  }
+
 
 
 include('inc/header.php');
 
 ?>
-<!-- Il y a une div id container autour du body  -->
 
 <!-- Bouton film -->
 <div class="more_films">
   <a href="index.php">Plus de films</a>
 </div>
-<!-- filtre catégories -->
 
-<div class="categories">
+
+<!-- Formulaire -->
 <form action="" method="post">
 
+  <!-- Selection date -->
+  <label for="selectionDate">Selectionner une periode</label>
+  <select class="" name="year">
+           <option value="none" selected="selected"></option>
+           <option value="1880-1889">1880-1889</option>
+           <option value="1890-1899">1890-1899</option>
+           <option value="1900-1909">1900-1909</option>
+           <option value="1910-1919">1910-1919</option>
+           <option value="1920-1929">1920-1929</option>
+           <option value="1930-1939">1930-1939</option>
+           <option value="1940-1949">1940-1949</option>
+           <option value="1950-1959">1950-1959</option>
+           <option value="1960-1969">1960-1969</option>
+           <option value="1970-1979">1970-1979</option>
+           <option value="1980-1989">1980-1989</option>
+           <option value="1990-1999">1990-1999</option>
+           <option value="2000-2009">2000-2009</option>
+           <option value="2010-2018">2010-2018</option>
+</select>
+
+
+
+  <!-- filtre catégories -->
+  <div class="categories">
   <ul><?php
     foreach ($tableau as $x) {
-
       echo '<li><input type="checkbox" name="" value="">'.$x.'</li>'
-
     ;}
-?></ul>
+
+?>
+
+<!-- Bouton rechercher -->
+<input type="submit" name="submitted" value="Rechercher">
+
+</ul>
 
 </form>
 
